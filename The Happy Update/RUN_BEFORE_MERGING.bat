b209a4691c49849c9629bc7a05540e2af613e099
LiquidSnackoo <codeviscerate@gmail.com>
2017-06-25 09:41:17 +0800

    Bat file changes

--- The Happy Update/RUN_BEFORE_MERGING.bat
+++ The Happy Update/RUN_BEFORE_MERGING.bat
@@ -6,4 +6,6 @@ rd /s /q "base\characters\Delia Kartmann"
 
 rd /s /q "base\characters\Apollo_Counsel" 
 
-
+echo MSGBOX "Bat file complete!" > %temp%\TEMPmessage.vbs
+call %temp%\TEMPmessage.vbs
+del %temp%\TEMPmessage.vbs /f /q
