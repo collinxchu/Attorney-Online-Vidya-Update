22f974fbc437083dde26a8fe00e57547d48311ec
NamuTelling <turtleswastaken@gmail.com>
2017-06-27 12:11:22 -0500

    Update RUN_BEFORE_MERGING.bat

--- The Happy Update/RUN_BEFORE_MERGING.bat
+++ The Happy Update/RUN_BEFORE_MERGING.bat
@@ -8,6 +8,8 @@ rd /s /q "base\characters\Apollo_Counsel"
 
 rename "base\sounds\music\[999] Investigation.mp3" "[999] Investigation (Middle).mp3"
 
+rename "base\characters\JudgeDGS" "Judge GB"
+
 
 echo MSGBOX "Bat file complete!" > %temp%\TEMPmessage.vbs
 call %temp%\TEMPmessage.vbs
