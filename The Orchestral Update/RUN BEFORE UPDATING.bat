bf06be395ed41d5126a23b1b2987a1dac89fb089
Phantom <you@example.com>
2017-08-29 01:06:23 +0100

    The Revamped Update

--- The Orchestral Update/RUN BEFORE UPDATING.bat
+++
@@ -1,6 +0,0 @@
-del /s /q "base\sounds\music\[DGS2] The Introduction of My Partner.mp3"
-
-
-echo MSGBOX "Bat file complete!" > %temp%\TEMPmessage.vbs
-call %temp%\TEMPmessage.vbs
-del %temp%\TEMPmessage.vbs /f /q
